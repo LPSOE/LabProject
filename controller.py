ffd1fe42ae0913561614b98487c1699e45b9eff5
LPSOE <lahpwemusoe@gmail.com>
2023-05-01 12:54:25 -0500

    First File

---
+++ controller.py
@@ -0,0 +1,8 @@
+from view import *
+from PyQt5.QtWidgets import *
+
+
+class Controller(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
