7563f3d75ce21b0ec55c851cf33481f5f9223113
TammyLe175 <ngochongle19@gmail.com>
2023-05-01 16:33:49 -0500

    add drink variable

--- controller.py
+++ controller.py
@@ -17,23 +17,26 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(lambda: self.submit())
         self.pushButton_2.clicked.connect(lambda: self.clear())
     def submit(self):
+
         tip = 0.00
         if self.ShawarmaRadioButton.isChecked():
             # self.BeefButton2.setHidden(False)
             # self.ChickenButton2.setHidden(False)
             if self.BeefButton2.isChecked() or self.ChickenButton2.isChecked():
                 if self.MildButton1.isChecked() or self.MediumButton1.isChecked() or self.SpicyButton1():
-                    if self.Tips1Button.isChecked():
-                        tip = 0.10
-                    elif self.Tips2Button.isChecked():
-                        tip = 0.15
-                    elif self.Tips3Button.isChecked():
-                        tip = 0.20
-                    tax = 15.00 * 0.1
-                    total_tip = (15.00 + tax) * tip
-                    total_bill = 15.00 + tax + total_tip
-                    self.receiptLabel.setText(f"\t Receipt \n Shawarma: $15.00 \t \t meat: $1.99 \t \t spice: mild, medium, spicy \t\t tax:{tax} \t \t total tip:{total_tip} \t\t total bill:{total_bill}")
+                    if self.WaterRadioButton.isChecked() or self.CokeRadioButton.isChecked() or self.PepsiRadioButton.isChecked():
+                        drink = 0.99
+                        if self.Tips1Button.isChecked():
+                            tip = 0.10
+                        elif self.Tips2Button.isChecked():
+                            tip = 0.15
+                        elif self.Tips3Button.isChecked():
+                            tip = 0.20
+                        tax = (15.00 + drink) * 0.1
+                        total_tip = (15.00 + tax + drink) * tip
+                        total_bill = 15.00 + tax + total_tip + drink
+                        self.receiptLabel.setText(f"\t Receipt \n Shawarma: $15.00 \n meat: $1.99 \n spice: mild, medium, spicy \n drink:{drink}(water, coke and pepsi) \n tax:{tax} \n total tip:{total_tip} \n total bill:{total_bill}")
 
     def clear(self):
-        self.receiptLabel("")
+        self.receiptLabel.setText("")
 
