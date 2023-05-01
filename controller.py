d5cbc8fdb1c122ba5a270977c1b79c7184108113
TammyLe175 <ngochongle19@gmail.com>
2023-05-01 15:45:56 -0500

    add submit function

--- controller.py
+++ controller.py
@@ -6,3 +6,34 @@ class Controller(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+
+        self.PhoRadioButton.setChecked(True)
+        self.MildButton1.setChecked(True)
+        self.WaterRadioButton.setChecked(True)
+        self.Tips1Button.setChecked(True)
+
+
+
+        self.pushButton.clicked.connect(lambda: self.submit())
+        self.pushButton_2.clicked.connect(lambda: self.clear())
+    def submit(self):
+        tip = 0.00
+        if self.ShawarmaRadioButton.isChecked():
+            # self.BeefButton2.setHidden(False)
+            # self.ChickenButton2.setHidden(False)
+            if self.BeefButton2.isChecked() or self.ChickenButton2.isChecked():
+                if self.MildButton1.isChecked() or self.MediumButton1.isChecked() or self.SpicyButton1():
+                    if self.Tips1Button.isChecked():
+                        tip = 0.10
+                    elif self.Tips2Button.isChecked():
+                        tip = 0.15
+                    elif self.Tips3Button.isChecked():
+                        tip = 0.20
+                    tax = 15.00 * 0.1
+                    total_tip = (15.00 + tax) * tip
+                    total_bill = 15.00 + tax + total_tip
+                    self.receiptLabel.setText(f"\t Receipt \n Shawarma: $15.00 \t \t meat: $1.99 \t \t spice: mild, medium, spicy \t\t tax:{tax} \t \t total tip:{total_tip} \t\t total bill:{total_bill}")
+
+    def clear(self):
+        self.receiptLabel("")
+
